b44f9602f1c48a448542376d7b88fb2caeca6976
Viacheslav_Valitskas <valitskas@gmail.com>
2018-04-24 00:53:32 +0300

    First commit

---
+++ app.mjs
@@ -0,0 +1,9 @@
+import mod from './config/module'
+import User from './models/User'
+import Product from './models/Product'
+
+console.log(mod.name);
+const user = new User();
+const product = new Product();
+const PI = 3.141593;
+console.log(`PI = ${PI}`);
